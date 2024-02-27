58b4812bb53b5edc13378155d5239fd857e6e1b9
xkruma01 <xkruma01@vutbr.cz>
2024-02-27 23:08:38 +0100

    [ENTITIES] ActivityEntity attributes

--- src/SchoolSystem/SchoolSystem.DAL/Entities/ActivityEntity.cs
+++ src/SchoolSystem/SchoolSystem.DAL/Entities/ActivityEntity.cs
@@ -1,12 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SchoolSystem.DAL.Entities
+namespace SchoolSystem.DAL.Entities;
+
+public record ActivityEntity : IEntity
 {
-    public record ActivityEntity
-    {
-    }
+    public DateTime Start { get; set; }
+    public DateTime End { get; set; }
+    public int ActivityType { get; set; }
+    public string? Description { get; set;  }
+
+    public required Guid SubjectId { get; set; }
+    public ICollection<EvaluationEntity> Evaluations { get; set; } = new List<EvaluationEntity>();
+
+    public Guid Id { get; set; }
 }
+
