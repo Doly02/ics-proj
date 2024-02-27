8a6c1d509f936e9fbe3efc2e843364bd34ed6c61
xteich02 <xteich02@stud.fit.vut.cz>
2024-02-27 19:12:44 +0100

    [GENERAL] Add attribute to Evaluation and establish association between Evaluation and Student

--- src/SchoolSystem/SchoolSystem.DAL/Entities/EvaluationEntity.cs
+++ src/SchoolSystem/SchoolSystem.DAL/Entities/EvaluationEntity.cs
@@ -6,7 +6,14 @@ using System.Threading.Tasks;
 
 namespace SchoolSystem.DAL.Entities
 {
-    public record EvaluationEntity
+    public record EvaluationEntity : IEntity
     {
+        public Guid Id { get; set; }
+        public int Score { get; set; }
+        public string? Note { get; set; }
+        public required StudentEntity Student { get; set; }
+
+
+
     }
 }
