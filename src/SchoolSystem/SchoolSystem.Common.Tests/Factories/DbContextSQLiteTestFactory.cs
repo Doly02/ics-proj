faba298059cbcd2150cd1dec58c8649e2a165d62
Tomas Dolak <xdolak09@vutbr.cz>
2024-03-01 17:00:56 +0100

    [TEST_DB] Initial Of Database Tests -> Factories, DbContext, Base

---
+++ src/SchoolSystem/SchoolSystem.Common.Tests/Factories/DbContextSQLiteTestFactory.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SchoolSystem.Common.Tests.Factories;
+
+public class DbContextSqLiteTestingFactory(string databaseName, bool seedTestingData = false)
+    : IDbContextFactory<SchoolSystemDbContext>
+{
+    public SchoolSystemDbContext CreateDbContext()
+    {
+        DbContextOptionsBuilder<SchoolSystemDbContext> builder = new();
+        builder.UseSqlite($"Data Source={databaseName};Cache=Shared");
+
+        // builder.LogTo(Console.WriteLine); //Enable in case you want to see tests details, enabled may cause some inconsistencies in tests
+        // builder.EnableSensitiveDataLogging();
+
+        return new SchoolSystemTestingDbContext(builder.Options, seedTestingData);
+    }
+}
---
+++ src/SchoolSystem/SchoolSystem.DAL.Tests/DbContextActivityTests.cs
@@ -0,0 +1,35 @@
+﻿//using SchoolSystem.Common.Tests.Seeds;
+using SchoolSystem.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace  SchoolSystem.DAL.Tests;
+
+public class DbContextActivityTests(ITestOutputHelper output) : DbContextTestsBase(output)
+{
+    [Fact]
+    public async Task_AddNewActivity_Per()
+    {
+        //Arrange
+        ActivityEntity entity = new()
+        {
+            Id = Guid.Parse("C5DE45D7-64A0-4E8D-AC7F-BF5CFDFB0EFC"),
+            Start = new DateTime(2023, 10, 1, 14, 0, 0),    // Set Start Date
+            End = new DateTime(2023, 10, 1, 16, 0, 0),      // Set End Date
+            Place = "101B", 
+            ActivityType = ActivityType.Lecture, 
+            Description = "Introduction to Programming" 
+        };
+
+        //Act
+        SchoolSystemDbContextSUT.Ingredients.Add(entity);
+        await SchoolSystemDbContextSUT.SaveChangesAsync();
+
+        //Assert
+        await using var dbx = await DbContextFactory.CreateDbContextAsync();
+        var actualEntities = await dbx.Activities.SingleAsync(i => i.Id == entity.Id);
+        Assert.Equal(entity, actualEntities);
+    }
+}
+
