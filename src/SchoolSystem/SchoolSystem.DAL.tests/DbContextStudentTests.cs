9cb256992ab0f603dc5abe2526de3954e08c6f09
xkruma01 <xkruma01@vutbr.cz>
2024-03-01 18:14:20 +0100

    [TESTDB] Added test, not running, xunit 2.4.0

--- src/SchoolSystem/SchoolSystem.DAL.tests/DbContextStudentTests.cs
+++ src/SchoolSystem/SchoolSystem.DAL.tests/DbContextStudentTests.cs
@@ -1,12 +1,32 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+//using SchoolSystem.Common.Tests.Seeds;
+using SchoolSystem.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+using Xunit.Abstractions;
 
 namespace SchoolSystem.DAL.Tests;
 
-public class DbContextStudentTests
+public class DbContextStudentTests(ITestOutputHelper output) : DbContextTestsBase(output)
 {
+    [Fact]
+    public async Task AddNew_Student_Persisted()
+    {
+        //Arrange
+        StudentEntity entity = new()
+        {
+            Id = Guid.Parse("C5DE45D7-64A0-4E8D-AC7F-BF5CFDFB0EFC"),
+            Name = "John",
+            Surname = "Doe"
+        };
+
+        //Act
+        SchoolSystemDbContextSUT.Students.Add(entity);
+        await SchoolSystemDbContextSUT.SaveChangesAsync();
+
+        //Assert
+        await using var dbx = await DbContextFactory.CreateDbContextAsync();
+        var actualEntities = await dbx.Students.SingleAsync(i => i.Id == entity.Id);
+        Assert.Equal(entity, actualEntities);
+    }
 }
 
