f88cd37e1527548c266618c279e5335e653736a9
xzahra33 <xzahra33@vutbr.cz>
2024-03-19 19:12:18 +0100

    [DAL] Adding dir UnitOfWork

---
+++ src/SchoolSystem/SchoolSystem.DAL/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,22 @@
+﻿using SchoolSystem.DAL.Entities;
+using SchoolSystem.DAL.Mappers;
+using SchoolSystem.DAL.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace SchoolSystem.DAL.UnitOfWork
+{
+    public sealed class UnitOfWork(DbContext dbContext) : IUnitOfWork
+    {
+        private readonly DbContext _dbContext =
+            dbContext ?? throw new ArgumentNullException(nameof(dbContext));
+
+        public IRepository<TEntity> GetRepository<TEntity, TEntityMapper>()
+            where TEntity : class, IEntity
+            where TEntityMapper : IEntityMapper<TEntity>, new()
+            => new Repository<TEntity>(_dbContext, new TEntityMapper());
+
+        public async Task CommitAsync() => await _dbContext.SaveChangesAsync();
+
+        public async ValueTask DisposeAsync() => await _dbContext.DisposeAsync();
+    }
+}
---
+++ src/SchoolSystem/SchoolSystem.DAL/UnitOfWork/UnitOfWorkFactory.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SchoolSystem.DAL.UnitOfWork
+{
+    public class UnitOfWorkFactory(IDbContextFactory<SchoolSystemDbContext> dbContextFactory)
+        : IUnitOfWorkFactory
+    {
+        public IUnitOfWork Create() => new UnitOfWork(dbContextFactory.CreateDbContext());
+    }
+}
