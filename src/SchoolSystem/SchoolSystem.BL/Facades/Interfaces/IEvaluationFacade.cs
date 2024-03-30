9192ed25b9a9d63527cc70d336c5d7db48ac030c
Tomas Dolak <xdolak09@vutbr.cz>
2024-03-30 15:12:53 +0100

    [FACADES] Implements Models - Interfaces of Entities In BL

---
+++ src/SchoolSystem/SchoolSystem.BL/Facades/Interfaces/IEvaluationFacade.cs
@@ -0,0 +1,11 @@
+﻿using SchoolSystem.BL.Models;
+using SchoolSystem.DAL.Entities;
+
+namespace SchoolSystem.BL.Facades;
+
+// Do Not Inherits From IEntity
+public interface IEvaluationFacade
+{
+    Task SaveAsync(EvaluationDetailModel model, Guid recipeId);
+    Task DeleteAsync(Guid id);
+}
---
+++ src/SchoolSystem/SchoolSystem.BL/Facades/Interfaces/IFacade.cs
@@ -0,0 +1,36 @@
+﻿using SchoolSystem.BL.Models;
+using SchoolSystem.DAL.Entities;
+
+namespace SchoolSystem.BL.Facades;
+
+// Base Interface 
+public interface IFacadeBase<TEntity> where TEntity : class, IEntity
+{
+    Task DeleteAsync(Guid id);
+}
+
+// Extended Interface 
+public interface IListModelFacade<TEntity, TListModel> : IFacadeBase<TEntity>
+    where TEntity : class, IEntity
+    where TListModel : IModel
+{
+    Task<IEnumerable<TListModel>> GetAsync();
+}
+// Extended Interface 
+public interface IDetailModelFacade<TEntity, TDetailModel> : IFacadeBase<TEntity>
+    where TEntity : class, IEntity
+    where TDetailModel : class, IModel
+{
+    Task<TDetailModel?> GetAsync(Guid id);
+    Task<TDetailModel> SaveAsync(TDetailModel model);
+}
+
+// Full Interface 
+public interface IFacade<TEntity, TListModel, TDetailModel> : IListModelFacade<TEntity, TListModel>, IDetailModelFacade<TEntity, TDetailModel>
+    where TEntity : class, IEntity
+    where TListModel : IModel
+    where TDetailModel : class, IModel
+{
+    // This Interface Inherits All Operations From IListModelFacade and IDetailModelFacade.
+    // No Additional Methods are Needed Here.
+}
