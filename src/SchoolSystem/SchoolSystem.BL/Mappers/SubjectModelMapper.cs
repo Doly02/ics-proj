64f9bf09e5da405c3ecabd63383ba2a15916ddae
Tomas Dolak <xdolak09@vutbr.cz>
2024-03-31 12:06:04 +0200

    [FACADES] Adds Base Implementation of Student & Subject Facades

--- src/SchoolSystem/SchoolSystem.BL/Mappers/SubjectModelMapper.cs
+++ src/SchoolSystem/SchoolSystem.BL/Mappers/SubjectModelMapper.cs
@@ -8,16 +8,16 @@ public class SubjectModelMapper : ModelMapperBase<SubjectEntity, SubjectListMode
     public override SubjectListModel MapToListModel(SubjectEntity? entity)
         => entity is null
             ? SubjectListModel.Empty
-            : new SubjectListModel { Id = entity.Id, Name = entity.Name, Surname = entity.Surname };
+            : new SubjectListModel { Id = entity.Id, Name = entity.Name, Abbreviation = entity.Abbreviation };
 
     public override SubjectDetailModel MapToDetailModel(SubjectEntity? entity)
         => entity is null
             ? SubjectDetailModel.Empty
             : new SubjectDetailModel
             {
-                Id = entity.Id, Name = entity.Name, Surname = entity.Surname, ImageUrl = entity.ImageUrl
+                Id = entity.Id, Name = entity.Name, Abbreviation = entity.Abbreviation
             };
 
     public override SubjectEntity MapToEntity(SubjectDetailModel model)
-        => new() { Id = model.Id, Name = model.Name, Surname = model.Surname, ImageUrl = model.ImageUrl };
+        => new() { Id = model.Id, Name = model.Name, Abbreviation = model.Abbreviation };
 }
