6fee8e90150623b4f50fd8fd64daeff837b72949
xkruma01 <xkruma01@vutbr.cz>
2024-04-03 20:59:27 +0200

    [MAPPERS/BL] Activity, Evaluation

---
+++ src/SchoolSystem/SchoolSystem.BL/Mappers/ActivityModelMapper.cs
@@ -0,0 +1,57 @@
+﻿using SchoolSystem.BL.Models;
+using SchoolSystem.DAL.Entities;
+
+namespace SchoolSystem.BL.Mappers;
+
+public class ActivityModelMapper : 
+    ModelMapperBase<ActivityEntity, ActivityListModel, ActivityDetailModel>,IActivityModelIMapper
+{
+    public override ActivityListModel MapToListModel(ActivityEntity? entity)
+        => entity is null
+            ? ActivityListModel.Empty
+            : new ActivityListModel
+            {
+                Id = entity.Id, 
+                Name = entity.Name,
+                // StudentFullName = 
+            };
+
+    public override ActivityDetailModel MapToDetailModel(ActivityEntity? entity)
+        => entity is null
+            ? ActivityDetailModel.Empty
+            : new ActivityDetailModel
+            {
+                Id = entity.Id, 
+                Name = entity.Name, 
+                ActivityType = entity.ActivityType, 
+                Description = entity.Description,
+                End = entity.End,
+                Place = entity.Place,
+                Start = entity.Start,
+                SubjectName = null!
+            };
+    
+    public ActivityListModel MapToListModel(ActivityDetailModel detailModel)
+        => new()
+            {
+                Id = detailModel.Id, 
+                Name = detailModel.Name,
+                // StudentFullName = 
+            };
+    
+    public override ActivityEntity MapToEntity(ActivityDetailModel model)
+        => throw new NotImplementedException("This method is unsupported. Use the other overload.");
+
+    public  ActivityEntity MapToEntity(ActivityDetailModel model, Guid subjectId)
+        => new() { Id = model.Id, 
+            Name = model.Name, 
+            ActivityType = model.ActivityType, 
+            Description = model.Description,
+            End = model.End,
+            Place = model.Place,
+            Start = model.Start,
+            Subject = null!,
+            Evaluations = Array.Empty<EvaluationEntity>(),
+            SubjectId = subjectId 
+        };
+}
---
+++ src/SchoolSystem/SchoolSystem.BL/Mappers/Interfaces/IEvaluationModelIMapper.cs
@@ -0,0 +1,10 @@
+﻿using SchoolSystem.BL.Models;
+using SchoolSystem.DAL.Entities;
+
+namespace SchoolSystem.BL.Mappers;
+
+public interface IEvaluationModelIMapper
+    : IModelMapper<EvaluationEntity, ActivityEvaluationListModel, EvaluationDetailModel>
+{
+    EvaluationEntity MapToEntity(EvaluationDetailModel model, Guid studentId); // for student id
+}
