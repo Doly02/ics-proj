88411215cb47c393079e01ad39d5df41befa338a
xkruma01 <xkruma01@vutbr.cz>
2024-04-05 16:11:59 +0200

    [FACADES] Add Enrolled

---
+++ src/SchoolSystem/SchoolSystem.BL/Facades/EnrolledFacade.cs
@@ -0,0 +1,15 @@
+﻿using SchoolSystem.BL.Mappers;
+using SchoolSystem.BL.Models;
+using SchoolSystem.DAL.Entities;
+using SchoolSystem.DAL.Mappers;
+using SchoolSystem.DAL.UnitOfWork;
+
+namespace SchoolSystem.BL.Facades;
+
+public class EnrolledFacade(
+    IUnitOfWorkFactory unitOfWorkFactory,
+    EnrolledModelMapper enrolledModelMapper)
+    :
+        FacadeBase<EnrolledEntity, EnrolledSubjectsListModel, SubjectDetailModel,
+            EnrolledEntityMapper>(unitOfWorkFactory, enrolledModelMapper);
+
