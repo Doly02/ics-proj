c97f24c58136ddb9c1cce34c8aa12d420277a85e
xsimun05 <xsimun05@vutbr.cz>
2024-04-20 10:50:07 +0200

    [FACADES] SubjectFacade search

--- src/SchoolSystem/SchoolSystem.BL/Facades/SubjectFacade.cs
+++ src/SchoolSystem/SchoolSystem.BL/Facades/SubjectFacade.cs
@@ -1,3 +1,4 @@
+using Microsoft.EntityFrameworkCore;
 using SchoolSystem.BL.Mappers;
 using SchoolSystem.BL.Models;
 using SchoolSystem.DAL.Entities;
@@ -6,9 +7,35 @@ using SchoolSystem.DAL.UnitOfWork;
 
 namespace SchoolSystem.BL.Facades;
 
-public class SubjectFacade(   
+public class SubjectFacade(
     IUnitOfWorkFactory unitOfWorkFactory,
     SubjectModelMapper modelMapper)
     :
 FacadeBase<SubjectEntity, SubjectListModel, SubjectDetailModel, SubjectEntityMapper>(
-    unitOfWorkFactory, modelMapper), ISubjectFacade;
+    unitOfWorkFactory, modelMapper), ISubjectFacade
+{
+    public async Task<IEnumerable<SubjectListModel>> SearchAsync(string? Name = null, Guid? Id = null)
+    {
+        await using var uow = UnitOfWorkFactory.Create();
+        IQueryable<SubjectEntity> query = uow.GetRepository<SubjectEntity, SubjectEntityMapper>().Get();
+
+        if (!string.IsNullOrEmpty(Name))
+        {
+            query = query.Where(e => EF.Functions.Like(e.Name, $"%{Name}%"));
+        }
+
+        if (Id.HasValue)
+        {
+            query = query.Where(e => e.Id == Id.Value);
+        }
+
+        foreach (string includePath in IncludesNavigationPathDetail)
+        {
+            query = query.Include(includePath);
+        }
+
+        List<SubjectEntity> entities = await query.ToListAsync();
+
+        return entities.Select(entity => modelMapper.MapToListModel(entity)).ToList();
+    }
+}
