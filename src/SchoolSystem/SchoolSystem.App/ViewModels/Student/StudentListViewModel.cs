ad3d476427588956ad6bb3c3a4a1861ef2e0eaf6
Tomas Dolak <xdolak09@vutbr.cz>
2024-04-27 19:09:16 +0200

    [VIEW_MODELS/Student] Implements Detail/Edit/List ViewModels

--- src/SchoolSystem/SchoolSystem.App/ViewModels/Student/StudentListViewModel.cs
+++ src/SchoolSystem/SchoolSystem.App/ViewModels/Student/StudentListViewModel.cs
@@ -1,6 +1,45 @@
-﻿namespace SchoolSystem.App.ViewModels;
+﻿using CommunityToolkit.Mvvm.Input;
+using CommunityToolkit.Mvvm.Messaging;
+using SchoolSystem.App.Messages;
+using SchoolSystem.App.Services;
+using SchoolSystem.BL.Facades;
+using SchoolSystem.BL.Models;
+ 
+namespace SchoolSystem.App.ViewModels;
 
-public class StudentListViewModel
+public partial class StudentListViewModel(
+    IStudentFacade studentFacade,
+    INavigationService navigationService,
+    IMessengerService messengerService)
+    : ViewModelBase(messengerService), IRecipient<StudentEditMessage>, IRecipient<StudentDeleteMessage>
 {
-    
+    public IEnumerable<StudentListModel> StudList { get; set; } = null!;
+    public async void Receive(StudentEditMessage message)
+    {
+        await LoadDataAsync();
+    }
+
+    public async void Receive(StudentDeleteMessage message)
+    {
+        await LoadDataAsync();
+    }
+
+    protected override async Task LoadDataAsync()
+    {
+        await base.LoadDataAsync();
+        StudList = await studentFacade.GetAsync();
+    }
+
+    [RelayCommand]
+    private async Task GoToCreateAsync()
+    {
+        await navigationService.GoToAsync("/edit");
+    }
+
+    [RelayCommand]
+    private async Task GoToDetailAsync(Guid id)
+    {
+        await navigationService.GoToAsync<StudentDetailViewModel>(new Dictionary<string, object?>
+            { [nameof(StudentDetailViewModel.Id)] = id });
+    }
 }
