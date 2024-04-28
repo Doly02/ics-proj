47b654f61f90231412941c74aa3140d6964a3433
xzahra33 <xzahra33@vutbr.cz>
2024-04-28 21:22:18 +0200

    [App] DetailViewModel + messages

--- src/SchoolSystem/SchoolSystem.App/ViewModels/Activity/ActivityDetailViewModel.cs
+++ src/SchoolSystem/SchoolSystem.App/ViewModels/Activity/ActivityDetailViewModel.cs
@@ -7,7 +7,37 @@ using SchoolSystem.BL.Models;
 
 namespace SchoolSystem.App.ViewModels.Activity;
 
-public partial class ActivityDetailViewModel
+public partial class ActivityDetailViewModel(
+    IActivityFacade activityFacade,
+    INavigationService navigationService,
+    IMessengerService messengerService)
+    : ViewModelBase(messengerService), IRecipient<ActivityEditMessage>
 {
+    public Guid Id { get; set; }
+    private ActivityDetailModel? Activity { get; set; }
     
+    protected override async Task LoadDataAsync()
+    {
+        await base.LoadDataAsync();
+
+        Activity = await activityFacade.GetAsync(Id);
+    }
+    
+    [RelayCommand]
+    private async Task GoToEditAsync()
+    {
+        if (Activity is not null)
+        {
+            await navigationService.GoToAsync("/edit",
+                new Dictionary<string, object?> { [nameof(ActivityEditViewModel.Activity)] = Activity with { } });
+        }
+    }
+    
+    public async void Receive(ActivityEditMessage message)
+    {
+        if (message.ActivityId == Activity?.Id)
+        {
+            await LoadDataAsync();
+        }
+    }
 }
