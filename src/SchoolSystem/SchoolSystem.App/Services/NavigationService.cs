dcc3a78cfeaf1c53d00c29fdb857a069f6c7f9ee
xkruma01 <xkruma01@vutbr.cz>
2024-04-30 18:31:04 +0200

    [SERVICES] Navigation for student, subject and activity

--- src/SchoolSystem/SchoolSystem.App/Services/NavigationService.cs
+++ src/SchoolSystem/SchoolSystem.App/Services/NavigationService.cs
@@ -1,6 +1,11 @@
 using SchoolSystem.App.Models;
 using SchoolSystem.App.ViewModels;
-// using SchoolSystem.App.Views...;
+using SchoolSystem.App.ViewModels.Activity;
+using SchoolSystem.App.Views.Student;
+// using SchoolSystem.App.Views.Subject;
+// using SchoolSystem.App.Views.Activity;
+// using SchoolSystem.App.Views.Enrolled;
+// using SchoolSystem.App.Views.Evaluation;
 
 namespace SchoolSystem.App.Services;
 
@@ -8,7 +13,24 @@ public class NavigationService : INavigationService
 {
     public IEnumerable<RouteModel> Routes { get; } = new List<RouteModel>
     {
-        // routes
+        // Home? would be before everything (//home/...)
+        //new("//home", typeof(HomeView), typeof(HomeViewModel)),
+        
+        // new("//students", typeof(StudentListView), typeof(StudentListViewModel)),
+        // new("//students/detail", typeof(StudentDetailView), typeof(StudentDetailViewModel)),
+        
+        new("//students/edit", typeof(StudentEditView), typeof(StudentEditViewModel)),
+        new("//students/detail/edit", typeof(StudentEditView), typeof(StudentEditViewModel)),
+        
+        // new("//subjects", typeof(SubjectListView), typeof(SubjectListViewModel)),
+        // new("//subjects/edit", typeof(SubjectEditView), typeof(SubjectEditViewModel)),
+        
+        // new("//subjects/activities", typeof(ActivityListView), typeof(ActivityListViewModel)),
+        // new("//subjects/activities/edit", typeof(ActivityEditView), typeof(ActivityEditViewModel)),
+        
+        // new("//subjects/activities/detail", typeof(ActivityDetailView), typeof(ActivityDetailViewModel)),
+        // new("//subjects/activities/detail/edit", typeof(ActivityEditView), typeof(ActivityEditViewModel)),
+        
     };
 
     public async Task GoToAsync<TViewModel>()
