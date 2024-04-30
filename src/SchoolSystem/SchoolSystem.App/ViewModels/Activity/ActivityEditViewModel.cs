4cbce79d2a757e0f62f7aa59a34da783bf7295df
xzahra33 <xzahra33@vutbr.cz>
2024-04-30 12:17:56 +0200

    [VIEW MODELS] Edit ActivityEditViewModel.cs

--- src/SchoolSystem/SchoolSystem.App/ViewModels/Activity/ActivityEditViewModel.cs
+++ src/SchoolSystem/SchoolSystem.App/ViewModels/Activity/ActivityEditViewModel.cs
@@ -1,15 +1,28 @@
-﻿using SchoolSystem.App.Services;
+﻿using CommunityToolkit.Mvvm.Input;
+using SchoolSystem.App.Messages;
+using SchoolSystem.App.Services;
 using SchoolSystem.BL.Facades;
 using SchoolSystem.BL.Models;
 
 namespace SchoolSystem.App.ViewModels.Activity;
 
+[QueryProperty(nameof(ViewModels.Activity), nameof(ViewModels.Activity))]
 public partial class ActivityEditViewModel(
     IActivityFacade activityFacade,
     INavigationService navigationService,
-    IMessengerService messengerService
-) : ViewModelBase(messengerService)
+    IMessengerService messengerService)
+    : ViewModelBase(messengerService)
 {
-    public ActivityDetailModel Activity { get; set; } = ActivityDetailModel.Empty;
-}
+    public ActivityDetailModel Activity { get; init; } = ActivityDetailModel.Empty;
 
+    // EDIT and SAVE
+    [RelayCommand]
+    private async Task SaveAsync()
+    {
+        await activityFacade.SaveAsync(Activity);
+
+        MessengerService.Send(new ActivityEditMessage { ActivityId = Activity.Id });
+
+        navigationService.SendBackButtonPressed();
+    }
+}
