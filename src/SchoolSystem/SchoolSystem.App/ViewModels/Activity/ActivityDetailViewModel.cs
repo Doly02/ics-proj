e4edd2f63ef7eb7b2a3081f8bccbb8ce858ab65f
xzahra33 <xzahra33@vutbr.cz>
2024-05-08 15:53:51 +0200

    [VIEW MODEL] [VIEW] Activity Detail

--- src/SchoolSystem/SchoolSystem.App/ViewModels/Activity/ActivityDetailViewModel.cs
+++ src/SchoolSystem/SchoolSystem.App/ViewModels/Activity/ActivityDetailViewModel.cs
@@ -7,15 +7,20 @@ using SchoolSystem.BL.Models;
 
 namespace SchoolSystem.App.ViewModels.Activity;
 
+[QueryProperty(nameof(Id), nameof(Id))]
 [QueryProperty(nameof(SubjectId), nameof(SubjectId))]
+
 public partial class ActivityDetailViewModel(
     IActivityFacade activityFacade,
     INavigationService navigationService,
-    IMessengerService messengerService)
-    : ViewModelBase(messengerService), IRecipient<ActivityEditMessage>
+    IMessengerService messengerService,
+    IAlertService alertService)
+    : ViewModelBase(messengerService), 
+        IRecipient<ActivityEditMessage>,
+        IRecipient<ActivityDeleteMessage>
 {
     public Guid Id { get; set; }
-    private ActivityDetailModel? Activity { get; set; }
+    public ActivityDetailModel? Activity { get; set; }
     public Guid SubjectId { get; set; }
     
     protected override async Task LoadDataAsync()
@@ -25,20 +30,54 @@ public partial class ActivityDetailViewModel(
     }
     
     [RelayCommand]
-    private async Task GoToEditAsync()
+    private async Task DeleteAsync()
     {
         if (Activity is not null)
         {
-            await navigationService.GoToAsync("/edit",
-                new Dictionary<string, object?> { [nameof(ActivityEditViewModel.Activity)] = Activity with { } });
+            try
+            {
+                await activityFacade.DeleteAsync(Activity.Id);
+                MessengerService.Send(new ActivityDeleteMessage());
+                navigationService.SendBackButtonPressed();
+            }
+            catch (InvalidOperationException)
+            {
+                await alertService.DisplayAsync("Operation Failed",
+                    "Removal of the Activity Failed.");
+            }
         }
     }
     
+    [RelayCommand]
+    private async Task GoToEditAsync()
+    {
+        await Shell.Current.GoToAsync("/edit",
+            new Dictionary<string, object?>
+            {
+                [nameof(ActivityEditViewModel.Id)] = Id,
+                [nameof(ActivityEditViewModel.SubjectId)] = SubjectId
+            });
+    }
+    
+    
+    [RelayCommand]
+    private async Task BackAsync()
+    {
+        await Shell.Current.GoToAsync("..");
+    }
+    
+    // After EDIT action
     public async void Receive(ActivityEditMessage message)
     {
         if (message.ActivityId == Activity?.Id)
         {
             await LoadDataAsync();
         }
+    }
+    
+    // After DELETE action
+    public async void Receive(ActivityDeleteMessage message)
+    {
+        await LoadDataAsync();
     }
 }
